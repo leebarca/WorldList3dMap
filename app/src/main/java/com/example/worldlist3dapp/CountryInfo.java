3e6174f60fa9a5c7b2be1c6b9ae911b96a8388f9
ldavison <ldavison@turnitin.com>
2024-10-28 06:02:46 +0000

    Improvements to layout

--- app/src/main/java/com/example/worldlist3dapp/CountryInfo.java
+++ app/src/main/java/com/example/worldlist3dapp/CountryInfo.java
@@ -3,12 +3,18 @@ package com.example.worldlist3dapp;
 public class CountryInfo {
     private String name;
     private int imageResId;
+    private String capital;
+    private String population;
+    private String language;
     private String description;
 
     // Constructor
-    public CountryInfo(String name, int imageResId, String description) {
+    public CountryInfo(String name, int imageResId, String capital, String population, String language, String description) {
         this.name = name;
         this.imageResId = imageResId;
+        this.capital = capital;
+        this.population = population;
+        this.language = language;
         this.description = description;
     }
 
@@ -19,6 +25,18 @@ public class CountryInfo {
 
     public int getImageResId() {
         return imageResId;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public String getLanguage() {
+        return language;
     }
 
     public String getDescription() {

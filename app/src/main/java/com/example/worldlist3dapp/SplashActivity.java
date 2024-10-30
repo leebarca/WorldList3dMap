7a4abf7705ae90ab24f894f04ea4d8ffb57bee71
ldavison <ldavison@turnitin.com>
2024-10-30 11:03:37 +0000

    Improvements to layout

---
+++ app/src/main/java/com/example/worldlist3dapp/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.worldlist3dapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DISPLAY_LENGTH = 2000; // 2 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Start MainActivity after the delay
+        new Handler().postDelayed(() -> {
+            Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(mainIntent);
+            finish(); // Close SplashActivity
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}

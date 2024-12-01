e5f461a3ae6a2d47c4b74bfa324da45013d30028
ldavison <ldavison@turnitin.com>
2024-12-01 20:44:30 +0000

    Improvements to layout

--- app/src/main/java/com/example/worldlist3dapp/ExploreActivity.java
+++ app/src/main/java/com/example/worldlist3dapp/ExploreActivity.java
@@ -1,6 +1,5 @@
 package com.example.worldlist3dapp;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,9 +15,9 @@ import java.util.List;
 
 public class ExploreActivity extends AppCompatActivity {
 
-    private CountryAdapter countryAdapter;
+    private OrderedCountriesAdapter adapter;
+    private ListView countryListView;
     private List<CountryInfo> countries;
-    private List<CountryInfo> filteredCountries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +25,47 @@ public class ExploreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_explore);
 
         // Initialize data
-        countries = CountryData.getCountriesInfo(this);
-        filteredCountries = new ArrayList<>();
 
         // ListView and Adapter
-        ListView countriesListView = findViewById(R.id.top_countries_list);
-        countryAdapter = new CountryAdapter(this, filteredCountries);
-        countriesListView.setAdapter(countryAdapter);
+        countryListView = findViewById(R.id.top_countries_list);
+        countries = CountryData.getCountriesInfo(this);
+        adapter = new OrderedCountriesAdapter(this, new ArrayList<>());
+        countryListView.setAdapter(adapter);
+
+        // Set OnItemClickListener for ListView
+        countryListView.setOnItemClickListener((parent, view, position, id) -> {
+            CountryInfo selectedCountry = adapter.getItem(position);
+
+            Intent intent = new Intent(ExploreActivity.this, CountryDetailActivity.class);
+            intent.putExtra("countryImageResId", selectedCountry.getImageResId());
+            intent.putExtra("countryName", selectedCountry.getName());
+            intent.putExtra("countryCapital", selectedCountry.getCapital());
+            intent.putExtra("countryPopulation", selectedCountry.getPopulation());
+            intent.putExtra("countryLanguage", selectedCountry.getLanguage());
+            intent.putExtra("countryReligion", selectedCountry.getReligion());
+            intent.putExtra("countryContinent", selectedCountry.getContinent());
+            intent.putExtra("countryArea", selectedCountry.getArea());
+            intent.putExtra("countryFlag", selectedCountry.getFlagResId());
+            intent.putExtra("countryDescription", selectedCountry.getDescription());
+            intent.putExtra("firstAttractionName", selectedCountry.getFirstAttractionName());
+            intent.putExtra("firstAttractionDetails", selectedCountry.getFirstAttractionDetails());
+            intent.putExtra("firstAttractionImage", selectedCountry.getFirstAttractionImage());
+            intent.putExtra("secondAttractionName", selectedCountry.getSecondAttractionName());
+            intent.putExtra("secondAttractionDetails", selectedCountry.getSecondAttractionDetails());
+            intent.putExtra("secondAttractionImage", selectedCountry.getSecondAttractionImage());
+            intent.putExtra("thirdAttractionName", selectedCountry.getThirdAttractionName());
+            intent.putExtra("thirdAttractionDetails", selectedCountry.getThirdAttractionDetails());
+            intent.putExtra("thirdAttractionImage", selectedCountry.getThirdAttractionImage());
+            intent.putExtra("bestTimeVisitArray", selectedCountry.getBestTimeVisit());
+            intent.putExtra("factInfo", selectedCountry.getFact());
+            intent.putExtra("websiteInfo", selectedCountry.getWebsite());
+            intent.putExtra("weatherInfo", selectedCountry.getWeather());
+            intent.putExtra("cuisineArray", selectedCountry.getCuisine());
+            intent.putExtra("safetyArray", selectedCountry.getSafety());
+            intent.putExtra("currency", selectedCountry.getCurrency());
+
+            startActivity(intent);
+        });
 
         // Spinner for category selection
         Spinner categorySpinner = findViewById(R.id.category_spinner);
@@ -46,12 +79,14 @@ public class ExploreActivity extends AppCompatActivity {
                 getString(R.string.dangerous_countries),
                 getString(R.string.safest_countries),
                 getString(R.string.largest_population_countries),
-                getString(R.string.smallest_population_countries)
+                getString(R.string.smallest_population_countries),
+                getString(R.string.most_visited_countries),
+                getString(R.string.least_visited_countries)
         };
 
         // Spinner Adapter
-        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, R.layout.spinner_dropdown_item, categories);
+        spinnerAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
         categorySpinner.setAdapter(spinnerAdapter);
 
         // Spinner item selection logic
@@ -73,34 +108,57 @@ public class ExploreActivity extends AppCompatActivity {
     }
 
     private void updateListView(String category) {
-        filteredCountries.clear();
+        // Create a new list to store filtered countries
+        List<CountryInfo> filteredCountries = new ArrayList<>();
 
+        // Filter countries based on the selected category
         if (category.equals(getString(R.string.biggest_countries))) {
-            for (CountryInfo country : countries) {
-                if (Integer.parseInt(country.getArea()) > 1000000) {
-                    filteredCountries.add(country);
-                }
-            }
+            List<String> country_list = List.of(getString(R.string.russia), getString(R.string.canada), getString(R.string.china), getString(R.string.united_states), getString(R.string.brazil));
+            filteredCountries = getOrderedCountries(country_list, countries);
         } else if (category.equals(getString(R.string.smallest_countries))) {
-            for (CountryInfo country : countries) {
-                if (Integer.parseInt(country.getArea()) < 1000) {
-                    filteredCountries.add(country);
+            List<String> country_list = List.of(getString(R.string.vatican_city), getString(R.string.monaco), getString(R.string.nauru), getString(R.string.tuvalu), getString(R.string.san_marino));
+            filteredCountries = getOrderedCountries(country_list, countries);
+        } else if (category.equals(getString(R.string.beach_countries))) {
+            List<String> country_list = List.of(getString(R.string.maldives), getString(R.string.seychelles), getString(R.string.thailand), getString(R.string.australia), getString(R.string.bahamas));
+            filteredCountries = getOrderedCountries(country_list, countries);
+        } else if (category.equals(getString(R.string.historic_countries))) {
+            List<String> country_list = List.of(getString(R.string.egypt), getString(R.string.greece), getString(R.string.italy), getString(R.string.china), getString(R.string.india));
+            filteredCountries = getOrderedCountries(country_list, countries);
+        } else if (category.equals(getString(R.string.dangerous_countries))) {
+            List<String> country_list = List.of(getString(R.string.yemen), getString(R.string.sudan), getString(R.string.south_sudan), getString(R.string.afghanistan), getString(R.string.drc_congo));
+            filteredCountries = getOrderedCountries(country_list, countries);
+        } else if (category.equals(getString(R.string.safest_countries))) {
+            List<String> country_list = List.of(getString(R.string.iceland), getString(R.string.ireland), getString(R.string.austria), getString(R.string.new_zealand), getString(R.string.singapore));
+            filteredCountries = getOrderedCountries(country_list, countries);
+        } else if (category.equals(getString(R.string.largest_population_countries))) {
+            List<String> country_list = List.of(getString(R.string.india), getString(R.string.china), getString(R.string.united_states), getString(R.string.indonesia), getString(R.string.pakistan));
+            filteredCountries = getOrderedCountries(country_list, countries);
+        } else if (category.equals(getString(R.string.smallest_population_countries))) {
+            List<String> country_list = List.of(getString(R.string.vatican_city), getString(R.string.tuvalu), getString(R.string.nauru), getString(R.string.palau), getString(R.string.san_marino));
+            filteredCountries = getOrderedCountries(country_list, countries);
+        } else if (category.equals(getString(R.string.most_visited_countries))) {
+            List<String> country_list = List.of(getString(R.string.france), getString(R.string.spain), getString(R.string.united_states), getString(R.string.china), getString(R.string.italy));
+            filteredCountries = getOrderedCountries(country_list, countries);
+        } else if (category.equals(getString(R.string.least_visited_countries))) {
+            List<String> country_list = List.of(getString(R.string.nauru), getString(R.string.tuvalu), getString(R.string.marshallislands), getString(R.string.kiribati), getString(R.string.micronesia));
+            filteredCountries = getOrderedCountries(country_list, countries);
+        }
+
+        // Update the adapter with the filtered list
+        adapter.updateCountries(filteredCountries);
+    }
+
+    private List<CountryInfo> getOrderedCountries(List<String> orderedNames, List<CountryInfo> allCountries) {
+        List<CountryInfo> orderedCountries = new ArrayList<>();
+        for (String name : orderedNames) {
+            for (CountryInfo country : allCountries) {
+                if (country.getName().equals(name)) {
+                    orderedCountries.add(country);
+                    break;
                 }
             }
-        } else if (category.equals(getString(R.string.beach_countries))) {
-            // Add your custom filtering logic for beach countries
-        } else if (category.equals(getString(R.string.historic_countries))) {
-            // Add your custom filtering logic for historic countries
         }
-        // ... Add logic for other categories
-
-        // Limit to top 5 results
-        if (filteredCountries.size() > 5) {
-            filteredCountries = filteredCountries.subList(0, 5);
-        }
-
-        // Notify adapter of changes
-        countryAdapter.updateCountries(filteredCountries);
+        return orderedCountries;
     }
 
     private void setupBottomNavigation() {
---
+++ app/src/main/java/com/example/worldlist3dapp/OrderedCountriesAdapter.java
@@ -0,0 +1,65 @@
+package com.example.worldlist3dapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderedCountriesAdapter extends ArrayAdapter<CountryInfo> {
+
+    private final List<CountryInfo> countries;
+
+    public OrderedCountriesAdapter(@NonNull Context context, @NonNull List<CountryInfo> countries) {
+        super(context, 0, countries);
+        this.countries = new ArrayList<>(countries);
+    }
+
+    @Override
+    public int getCount() {
+        return countries.size();
+    }
+
+    @Nullable
+    @Override
+    public CountryInfo getItem(int position) {
+        return countries.get(position);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_ordered_country_list_adapter, parent, false);
+        }
+
+        CountryInfo country = getItem(position);
+
+        TextView orderNumber = convertView.findViewById(R.id.order_number);
+        ImageView countryFlag = convertView.findViewById(R.id.country_flag);
+        TextView countryName = convertView.findViewById(R.id.country_name);
+
+        if (country != null) {
+            // Set order number
+            orderNumber.setText(String.valueOf(position + 1));
+
+            // Set country flag and name
+            countryFlag.setImageResource(country.getFlagResId());
+            countryName.setText(country.getName());
+        }
+
+        return convertView;
+    }
+
+    public void updateCountries(List<CountryInfo> newCountries) {
+        countries.clear();
+        countries.addAll(newCountries);
+        notifyDataSetChanged();
+    }
+}

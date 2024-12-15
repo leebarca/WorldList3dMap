dcfbf2429fd062a4c9f15f3c2b743327603a77c8
ldavison <ldavison@turnitin.com>
2024-12-15 16:36:41 +0000

    Improvements to layout

--- app/src/main/java/com/leebarcaglobal/worldtravel3d/MainActivity.java
+++ app/src/main/java/com/leebarcaglobal/worldtravel3d/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.ListView;
 import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import androidx.core.splashscreen.SplashScreen;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -38,6 +39,8 @@ public class MainActivity extends BaseActivity {
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
